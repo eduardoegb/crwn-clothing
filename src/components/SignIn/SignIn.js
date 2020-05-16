54b854fe46a5db40d39addabeb50b6528c83e63d
Eduardo Gonzalez Briceño <eduardoegb@gmail.com>
2020-05-16 10:54:42 -0400

    Finished sign-in and sign-up components

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../FormInput/FormInput';
 import CustomButton from '../CustomButton/CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase';
+import { auth, signInWithGoogle } from '../../firebase/firebase';
 
 import './SignIn.scss';
 
@@ -17,9 +17,18 @@ class SignIn extends Component {
 		}
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
-		this.setState({email: '', password: ''})
+
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({email: '', password: ''})
+		} catch (error) {
+			console.log(error)
+		}
+
 	}
 
 	handleChange = (event) => {
